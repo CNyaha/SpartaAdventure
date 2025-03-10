8378042c405487b87803c9bc12eb1cbe0dd4b8c3
CNyaha <llhb0625@gmail.com>
2025-03-10 09:50:26 +0900

    feat_캐릭터 추가 및 이동 및 움직임 추가

---
+++ Assets/Scripts/UI/Condition.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Condition : MonoBehaviour
+{
+    public float curValue;
+    public float startValue;
+    public float maxValue;
+    public float passiveValue;
+    public Image uiBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curValue = startValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        uiBar.fillAmount = GetPercentage();
+    }
+
+    float GetPercentage()
+    {
+        return curValue / maxValue;
+    }
+}
